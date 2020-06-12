4d77d0f4d0fe1926280d249b3a0e3cc62fd8f8f6
Jeremiah Sandahl <jsandahl@zanthion.com>
2020-06-11 22:30:40 -0500

    initial commit

---
+++ api/api_library/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Book(models.Model):
+    title=models.CharField(max_length=30)
+    author=models.CharField(max_length=100)
+    recommender=models.CharField(max_length=100)
+    source=models.TextField()
+    amazon_link=models.TextField()
+    description=models.TextField()
+    book_type=models.TextField()
+    genrer=models.TextField()
+    length=models.IntegerField()
+    publish_year=models.IntegerField()
+    on_list=models.TextField()
+    review_excerpt=models.TextField()
