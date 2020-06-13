b3fd31225cfd0871160699c30676273c6a85510e
Jeremiah Sandahl <jsandahl@zanthion.com>
2020-06-12 19:48:36 -0500

    Swapped out for postgres backend to allow for ArrayAgg for deduplication

--- api/api_library/models.py
+++ api/api_library/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Book(models.Model):
-    title=models.CharField(max_length=30)
+    title=models.CharField(max_length=256)
     author=models.CharField(max_length=100)
     recommender=models.CharField(max_length=100)
     source=models.TextField()
